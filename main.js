bc360f3e3481e5841c67525df831566c4076fe54
TaylorAkin <taylorakin.ky@gmail.com>
2019-09-10 23:16:20 -0400

    updating master

--- main.js
+++ main.js
@@ -6,14 +6,18 @@ var countDownDate = new Date('April 23, 2020 23:45:00');
 function countDownTimer() {
     //variable set to current time
     var currentTime = new Date().getTime();
+
+    //difference is all the milliseconds, from getTime, between currentime and countDownDate
     var difference = countDownDate.getTime() - currentTime;
+
+    //finding total amount for each unit of time
     var totalmilliseconds = difference;
     var totalseconds = Math.floor(totalmilliseconds / 1000);
     var totalminutes = Math.floor(totalseconds / 60);
     var totalhours = Math.floor(totalminutes / 60);
     var totaldays = Math.floor(totalhours / 24);
 
-
+//findind the remainder left for each unit of time
     var rmillisec = totalmilliseconds % 1000;
     var rseconds = totalseconds % 60;
     var rminutes = totalminutes % 60;
@@ -22,12 +26,12 @@ function countDownTimer() {
    
 
 
-
+// plugging the reaminders for each unit into the demo id of the html. Also concatinating the strings to name each number
     document.getElementById("demo").innerHTML =  totaldays + " Days "  + rhours + " Hour " + rminutes + " Minutes: " + rseconds + " Seconds " +  rmillisec +" Milliseconds ";
 }
 
 
-//setting the interval I want my function for countdown to run at, every second. 
+//setting the interval I want my function for countdown to run at, every second. (This is seen by javascript before it runs the function)
 var  countDownInterval = setInterval(countDownTimer, 1000);
 
 
@@ -36,8 +40,6 @@ var  countDownInterval = setInterval(countDownTimer, 1000);
 
 
 
-    // countDownDate - currentTime
-    // display that to html
 
 
 
